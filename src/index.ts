71c267f9a1f26d980f2ab28afe87d15725dc33e6
WMXPY <wm@mengw.io>
2019-11-05 12:26:30 -0600

    init files

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@
  * @description Index
  */
 
+export * from "./pile";
 
--- src/pile.ts
+++ src/pile.ts
@@ -17,4 +17,36 @@ export class Pile<T extends any> {
 
         this._pile = new Map<string, T>();
     }
+
+    public get length(): number {
+
+        return this._pile.size;
+    }
+
+    public add(key: string, value: T): this {
+
+        this._pile.set(key, value);
+        return this;
+    }
+
+    public clear(): this {
+
+        this._pile.clear();
+        return this;
+    }
+
+    public assertGet(key: string): T {
+
+        const value: T | undefined = this.get(key);
+        if (!value) {
+            throw new Error('[Sudoo-Pile] Undefined Value');
+        }
+
+        return value;
+    }
+
+    public get(key: string): T | undefined {
+
+        return this._pile.get(key);
+    }
 }
